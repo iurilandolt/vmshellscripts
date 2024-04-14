75b8836d23f280743eee0e80bb45cb57e3875e3d
iuri- <123178126+iurilandolt@users.noreply.github.com>
2024-04-15 00:39:05 +0100

    update

--- envsetup-01.sh
+++ envsetup-01.sh
@@ -19,7 +19,7 @@ git clone https://git.suckless.org/st /tmp/st
 
 sudo make -C  /tmp/dwm clean install
 sudo make -C  /tmp/st clean install
-sed -i 's/"\/bin\/sh"/"\/usr\/local\/bin\/st"/'
+sed -i 's/"\/bin\/sh"/"\/usr\/local\/bin\/st"/' /tmp/st/config.h
 
 
 #echo "exec dwn" >> ~/.xinitrc
