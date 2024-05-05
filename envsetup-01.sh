baa4efafb9b4d4a03ae2fe82522d729a591656e3
iuri- <123178126+iurilandolt@users.noreply.github.com>
2024-05-05 13:12:04 +0100

    auto

--- envsetup-01.sh
+++ envsetup-01.sh
@@ -1,17 +1,16 @@
-pacman -Syu
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-pacman -S --noconfirm xf86-video-fbdev #xf86-video-vmware xf86-video-qxl xf86-video-sisusb
+sudo pacman -Syu
+
+sudo pacman -S --noconfirm xf86-video-fbdev #xf86-video-vmware xf86-video-qxl xf86-video-sisusb
 
 pacman -S --noconfirm alsa-utils pulseaudio-alsa pulseaudio-equalizer
 mkdir -p /etc/pulse/default.pa.d
-echo "unload-module module-role-cork" >> /etc/pulse/default.pa.d/no-cork.pa
+sudo echo "unload-module module-role-cork" >> /etc/pulse/default.pa.d/no-cork.pa
 
 #https://archlinux.org/groups/x86_64/xorg/
 #https://archlinux.org/groups/x86_64/xorg-drivers/
-pacman -S --noconfirm xorg-server xorg-xinit xorg-xkill xorg-xsetroot xorg-xbacklight xorg-xprop \
-	xorg-xrandr xorg-xfontsel xorg-xlsfonts xorg-xinput xorg-xwininfo libx11 libxinerama libxft
-
-pacman -S --noconfirm vim
+sudo pacman -S --noconfirm xorg xorg-xinit nitrogen picom vim
 
 git clone https://git.suckless.org/dwm ~/.config/dwm
 git clone https://git.suckless.org/st ~/.config/st
@@ -24,7 +23,6 @@ cat /etc/X11/xinit/xinitrc > ~/.xinitrc
 echo "exec dwn" >> ~/.xinitrc
 
 #echo "startx" >> ~/.bash_profile
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # https://www.youtube.com/watch?v=SRqVuAUP2N0&ab_channel=BugsWriter
 # https://arch.d3sox.me/installation/install-desktop
