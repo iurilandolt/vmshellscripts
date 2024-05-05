caf2404de6c0599d6b2f785bd1d7837e87d391fa
iuri- <123178126+iurilandolt@users.noreply.github.com>
2024-05-05 14:12:22 +0100

    auto

--- envsetup-01.sh
+++ envsetup-01.sh
@@ -1,6 +1,6 @@
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-sudo git clone https://aur.archlinux.org.yay-git.git
+git clone https://aur.archlinux.org.yay-git.git
 cd yay-git/
 mkpkg -si
 cd ~
@@ -17,15 +17,15 @@ sudo echo "unload-module module-role-cork" >> /etc/pulse/default.pa.d/no-cork.pa
 #https://archlinux.org/groups/x86_64/xorg-drivers/
 sudo pacman -S --noconfirm xorg xorg-xinit nitrogen picom vim
 
-git clone https://git.suckless.org/dwm ~/.srcs/dwm
-git clone https://git.suckless.org/st ~/.srcs/st
+#git clone https://git.suckless.org/dwm ~/.srcs/dwm
+#git clone https://git.suckless.org/st ~/.srcs/st
 
-sudo make -C  ~/.srcs/dwm clean install
-sudo make -C  ~/.srcs/st clean install
-sed -i 's/"\/bin\/sh"/"\/usr\/local\/bin\/st"/' ~/.srcs/st/config.h
+#sudo make -C  ~/.srcs/dwm clean install
+#sudo make -C  ~/.srcs/st clean install
+#sed -i 's/"\/bin\/sh"/"\/usr\/local\/bin\/st"/' ~/.srcs/st/config.h
 
-cat /etc/X11/xinit/xinitrc > ~/.xinitrc
-echo "exec dwn" >> ~/.xinitrc
+#cat /etc/X11/xinit/xinitrc > ~/.xinitrc
+#echo "exec dwn" >> ~/.xinitrc
 
 #echo "startx" >> ~/.bash_profile
 
